995843f433d9c2e38a86abbf34459fd3eaa0385e
Henrik Raitasola <heka.fof@gmail.com>
2015-12-18 03:49:29 +0200

    Fixed search results layout on mobile.

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -9,7 +9,7 @@ export default class SearchResults extends Component {
 						backgroundImage: 'url(' + movie.Poster + ')'
 					};
 					return (
-						<div className="movie-card col col--1-of-5">
+						<div className="movie-card">
 							<div className="movie-card--cover" style={coverStyle} />
 							<h4 className="movie-card--title">{movie.Title}</h4>
 							<p className="movie-card--year">{movie.Year}</p>

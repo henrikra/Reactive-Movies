1da171822121e03385ed9458296103a68317abde
Henrik Raitasola <heka.fof@gmail.com>
2015-12-18 03:09:09 +0200

    Added basic styles to the page. Added scss loader to webpack. Search results are now rendered. User can infinitely load more results but there is a bug: first three sets are the same when you press "Show more" but after that user gets more actual results.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,70 @@ import React, { Component } from 'react';
 var $ = require('jquery');
 import SearchResults from './SearchResults';
 
-export default class App extends Component {
-	state = { searchURL: '' };
+require('./styles/style.scss')
 
-	search(searchURL, event) {
+export default class App extends Component {
+	state = {
+		searchURL: '',
+		searchResults: [],
+		page: 1,
+		query: ''
+	};
+
+	search = event => {
 		if (event.keyCode == 13) {
-			$.get(searchURL, function(result) {
-				console.log(result);
+			$.get(this.state.searchURL, function(result) {
+				console.log(result.Search);
+				this.setState({
+					searchResults: result.Search,
+					page: 1
+				});
 			}.bind(this));
 		}
 	}
 	updateSearchURL = event => {
-		var searchURLBase = 'http://www.omdbapi.com/?s=';
-		let { searchQuery } = event.target;
+		var searchURLBase = 'http://www.omdbapi.com/?page=' + this.state.page + '&s=';
+		this.state.query = event.target.value;
 		this.setState({
-			searchURL: searchURLBase + encodeURIComponent(searchQuery)
+			searchURL: searchURLBase + encodeURIComponent(this.state.query)
 		});
+	}
+	loadMoreResults = event => {
+		console.log("ennen " + this.state.page);
+		var newPage = this.state.page + 1;
+		this.setState({
+			page: newPage
+		});
+		console.log("jälkeen " + this.state.page);
+		var searchURLBase = 'http://www.omdbapi.com/?page=' + this.state.page + '&s=';
+		this.setState({
+			searchURL: searchURLBase + encodeURIComponent(this.state.query)
+		});
+		var currentSearchResults = this.state.searchResults;
+
+
+		$.get(this.state.searchURL, function(result) {
+			result.Search.forEach(function(movie) {
+				currentSearchResults.push(movie); // gfzizz
+			});
+			this.setState({
+				searchResults: currentSearchResults
+			});
+		}.bind(this));
 	}
   render() {
     return (
-    	<div className="container">
-      	<input type="search" onKeyUp={this.search.bind(this, this.state.searchURL)} onChange={this.updateSearchURL} />
-      	<p>{this.state.searchURL}</p>
-      	<SearchResults />
+    	<div className="wrap">
+	    	<nav>
+	    		<div className="container">
+	    			<input className="search-box" type="search" onKeyUp={this.search} onChange={this.updateSearchURL} />
+	    		</div>
+	    	</nav>
+	    	<div className="container">
+	      	<p>{this.state.searchURL}</p>
+	      	<SearchResults movies={this.state.searchResults} />
+	      	<button type="button" onClick={this.loadMoreResults} >Show more</button>
+	      </div>
       </div>
     );
   }
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 export default class SearchResults extends Component {
 	render() {
 		return (
-			<h1>Search results</h1>
+			<div className="row">
+				{this.props.movies.map(function(movie) {
+					var coverStyle = {
+						backgroundImage: 'url(' + movie.Poster + ')'
+					};
+					return (
+						<div className="movie-card col col--1-of-5">
+							<div className="movie-card--cover" style={coverStyle} />
+							<h4 className="movie-card--title">{movie.Title}</h4>
+							<p className="movie-card--year">{movie.Year}</p>
+						</div>
+					);
+				})}
+			</div>
 		);
 	}
 }

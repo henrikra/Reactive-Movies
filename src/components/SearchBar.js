4cc47a906825afcba6aae34118db0864d244ad71
Henrik Raitasola <heka.fof@gmail.com>
2015-12-23 06:25:39 +0200

    Improved search box usability with select and blur functions.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 export default class SearchBar extends Component {
 	state = {
@@ -16,10 +17,13 @@ export default class SearchBar extends Component {
 			return;
 		}
 		this.props.onQuerySubmit(query, type);
-		this.setState({query: ''});
+		$(this.refs.box).blur();
 	}
 	handleTypeChange = (event) => {
 		this.setState({type: event.target.options[event.target.selectedIndex].value});
+	}
+	handleQueryFocus(event) {
+		$(event.target).select();
 	}
 	render() {
 		return (
@@ -27,11 +31,14 @@ export default class SearchBar extends Component {
 				<form className="search-form" onSubmit={this.handleSubmit} >
 		  		<div className="container">
 		  			<input
+		  				ref="box"
 		  				className="search-form--box"
 		  				type="search"
 		  				placeholder="Search movies or series..."
 		  				value={this.state.query}
 		  				onChange={this.handleQueryChange}
+		  				onFocus={this.handleQueryFocus}
+		  				autoFocus
 		  			/>
 		  			<select value={this.state.type}
 		  				className="search-form--type"

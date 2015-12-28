668651a39ca3b76814200acd95e92f3f5ca5b9a2
Henrik Raitasola <heka.fof@gmail.com>
2015-12-28 17:55:02 +0200

    Added 3rd party component: React typeahead. Now when user types to search box the user will get movie suggestions.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+var Typeahead = require('react-typeahead').Typeahead;
 
 export default class SearchBar extends Component {
 	state = {
 		query: '',
-		type: ''
-	}
-	handleQueryChange = event => {
-		this.setState({query: event.target.value});
+		type: '',
+		options: []
 	}
 	handleSubmit = event => {
-		event.preventDefault();
+		if (event) {
+			event.preventDefault();
+		}
 		var query = this.state.query.trim();
 		var type = this.state.type.trim();
 		if (!query) {
 			return;
 		}
 		this.props.onQuerySubmit(query, type);
-		$(this.refs.box).blur();
+		$('.typeahead-selector').hide();
+		this.setState({type: ''});
 	}
 	handleTypeChange = (event) => {
 		this.setState({type: event.target.options[event.target.selectedIndex].value});
 	}
 	handleQueryFocus(event) {
 		$(event.target).select();
+	}
+	displayOptions(option, index) {
+		return option.Title;
+	}
+	getOptions = event => {
+		if (event.keyCode == 13) {
+			this.handleSubmit();
+			$(event.target).blur();
+		} else {
+			$('.typeahead-selector').show();
+			this.setState({query: event.target.value});
+			$.get('http://www.omdbapi.com/?s=' + event.target.value, (result) => {
+				if (result.Search) {
+					this.setState({options: result.Search });
+				} else {
+					this.setState({options: [] });
+				}
+			});
+		}
+	}
+	setSelectedOption = option => {
+		this.setState({query: option.Title});
 	}
 	render() {
 		var submitBtn;
@@ -36,16 +60,19 @@ export default class SearchBar extends Component {
 			<nav>
 				<form className="search-form" onSubmit={this.handleSubmit} >
 		  		<div className="container">
-		  			<input
-		  				ref="box"
-		  				className="search-form--box"
-		  				type="search"
-		  				placeholder="Search movies or series..."
-		  				value={this.state.query}
-		  				onChange={this.handleQueryChange}
-		  				onFocus={this.handleQueryFocus}
-		  				autoFocus
-		  			/>
+			  		<Typeahead
+					    options={this.state.options}
+					    maxVisible={10}
+					    filterOption="Title"
+					    displayOption={this.displayOptions}
+					    onKeyUp={this.getOptions}
+					    customClasses={
+					    	{input: 'search-form--box'}
+					    }
+					    onOptionSelected={this.setSelectedOption}
+					    placeholder="Search movies or series..."
+					    onFocus={this.handleQueryFocus}
+					  />
 		  			<select value={this.state.type}
 		  				className="search-form--type"
 		  				onChange={this.handleTypeChange}
